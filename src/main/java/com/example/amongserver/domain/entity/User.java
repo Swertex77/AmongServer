3efcf5b5b62d9dbf5b1fb1b79ed0b2185311cea6
AlexandrPopov <164056516+Aleck210popov@users.noreply.github.com>
2024-04-21 04:40:45 +0300

    User.login

--- src/main/java/com/example/amongserver/domain/entity/User.java
+++ src/main/java/com/example/amongserver/domain/entity/User.java
@@ -18,17 +18,17 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column (name = "latitude")
-    private String name;
+    @Column (name = "login")
+    private String login;
 
-    @Column (name = "longitude")
+    @Column (name = "is_ready")
     private boolean isReady;
 
-    @Column (name = "completed")
+    @Column (name = "is_imposter")
     private boolean isImposter;            ;
 
-    public User(String name, boolean isReady, boolean isImposter) {
-        this.name = name;
+    public User(String login, boolean isReady, boolean isImposter) {
+        this.login = login;
         this.isReady = isReady;
         this.isImposter = isImposter;
     }
